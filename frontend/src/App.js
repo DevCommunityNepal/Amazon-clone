b8043331ed645fd3b1024a168b2dd48de38f40b2
DEVCommunityNepal <devcommunitynepalofficial@gmail.com>
2023-03-07 21:34:05 +0545

    Routes Added with Params

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
-import data from "./data";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <a href="/"> Amazon </a>
+          <Link to="/"> Amazon </Link>
         </header>
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
           </Routes>
         </main>
       </div>
